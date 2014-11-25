8e2e18eb3da2e77faac0cffb1974a723196f1679
adellantado <adellantado@gmail.com>
2014-11-26 00:26:38 +0200

    Chain: filter, map, catch functions

--- src/Chain.js
+++ src/Chain.js
@@ -11,18 +11,24 @@ this.Chain = function(func) {
 
     var next;
 
+    var resolved;
+
     this.resolve = function(data) {
         var res;
-        if (resolveFunc)
+        if (resolveFunc) {
             res = resolveFunc(data);
+            resolved = true;
+        }
         if (next)
             next.resolve(res);
     }
 
     this.reject = function(data) {
         var res;
-        if (rejectFunc)
+        if (rejectFunc) {
             res = rejectFunc(data);
+            resolved = false;
+        }
         if (next)
             next.reject(res);
     }
@@ -34,10 +40,56 @@ this.Chain = function(func) {
     if (func)
         runFunc();
 
+    this.resolved = function() {
+        return resolved;
+    }
+
+    this.rejected = function() {
+        return !resolved;
+    }
+
     this.then = function(resolve, reject) {
 
         resolveFunc = resolve;
         rejectFunc = reject;
+
+        return next = new Chain();
+
+    }
+
+    this.catch = function(reject) {
+        rejectFunc = reject;
+
+        return next = new Chain();
+    }
+
+    this.map = function(map) {
+        resolveFunc = map;
+
+        return next = new Chain();
+
+    }
+
+    var nextSaved;
+    this.filter = function(filter) {
+
+        resolveFunc = function(data) {
+
+            var res = filter;
+            if (typeof filter == 'function')
+                res = filter(data);
+
+            if (!res)
+                if (next) {
+                    nextSaved = next;
+                    next = null;
+                }
+            else if (!next) {
+                next = nextSaved;
+                nextSaved = null;
+            }
+
+        }
 
         return next = new Chain();
 
